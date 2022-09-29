4cbfae1cb88d46fbda188fa801e1419e667735dc
Joey Garcia <MasterGamin4790040@gmail.com>
2022-09-29 18:40:01 -0400

    Updated and tweaked to be better before implementing (native) wimdow support.

--- README.md
+++ README.md
@@ -2,21 +2,20 @@
 This is a simple script that will automatically install and configures an extremely optimized vanilla-experience server for Minecraft Java Edition. Powered by the Quilt Mod Loader! This is the best script to automatically get the most optimized MC Server out there as far as I know. Add any Fabric or Quilt mod you want, and it Works in Termux too!
 
 ## Thanks to these things/people that made it possible !
-> MC 1.19.2 - Synced server package manifest to client, updating the network framework Netty
+> MC v1.19.2 - Synced server package manifest to client, updating the framework Netty
 
-> Quilt Mod Loader - Best solution for hosting MC servers imo. (That isn't enterprise, proprietary and specifically designed for certain things.)
+> Quilt Mod Loader - Best solution for hosting MC servers imo (That isn't enterprise, proprietary and designed for specific things.)
 
 > Quilt Mod Loader Discord - Very cool people and devs helped me find lib issues, ty.
 
 > The Mods - Lithium, VMP *(Very Many Players)*, FerriteCore, Krypton, ViaFabric, Starlight, qfapi *(fabric library implementation and Quilt library from Quilt)*, Fabrictailor *(to change skins, do /skin)*, Clumps, C2ME.
 
-> Sukiru - they're like a java dev as a job or something idk, they were useful nontheless with answering questions.
+> Sukiru - they're like a java dev or something, they were useful with answering my very urgent and serious questions.
 
-*Will update this repository accordingly if a new mc version comes out, and I will also update the mods time to time. Feel free to open issues and whatnot too :)*
 # Steps to Install
 
 ### ⚠️ This guide assumes you are runnning a supported operating system in termux. If not, you can download AnLinux to install a supported distro. ⚠️
-p.s As long as you are on a supported operating system, this script should work, even if you're not on Termux. Windows
+*p.s As long as you are on a supported operating system, this script should work, even if you're not on Termux. I Will update this repository accordingly if a new mc version comes out, and I will also update the mods and things from time to time. Ofcourse you can do this yourself, and I would reccomend to aswell, but you wont die if you dont so don't sweat.*
 
 **Currently supported distros; Fedora, Debian, Ubuntu, Arch Linux, Manjaro, Alpine Linux, and anything else that has the package manager found in these core distros.**
 
@@ -38,8 +37,9 @@ Fedora
 Alpine
 
 `apk add git`
+---------------------------------------------------------------------------------------------------------------------------------------
 
-## Cmds (run in order, do this to update your server whenever I make a new update.)
+## Install (run in order, do this to update your server whenever I make a new update.)
 `git clone https://github.com/Theslees/termux-Optimized-MC-Java-server mc && cd mc`
 
 `chmod +x one-time-install.sh`
@@ -53,7 +53,7 @@ Alpine
 
 ###### i am not responsible for batteries frying or anything that may happen. You are doing this on your own will, and you are aware of the Risks that come with putting your phone through this.
 
-# Windows
+## Windows Setup
 wip
 
 # To do
--- one-time-install.sh
+++ one-time-install.sh
@@ -1,56 +1,56 @@
 #!/bin/bash
-# Works with superuser and normal user. finally
+# Works with superuser and normal user. finally. https://github.com/Theslees/termux-Optimized-MC-Java-server
 printf "Setting up..."
 printf "\ncurrently running script as $USER\n "
-
 . /etc/os-release
+
 version="1.19.2"
-installer_version="0.4.3"
+installer_version="0.4.4"
 #bashrc variable removed, not used
 ram=$(free --mega | grep Mem | awk '{ print $7 }')
 whereami=$(pwd)
 #distroid removed, not needed
 mc="$HOME/.local/bin/mc"
 mc_root="/bin/mc"
-# Download dependencies and requirements
 
+# Download dependencies and requirements
 installer() {
-if [ -x "$(command -v apk)" ];       then  apk add --no-cache openjdk18-jre-headless
-elif [ -x "$(command -v apt-get)" ]; then  apt-get install openjdk-18-jre-headless openjdk-18-jdk-headless tar
-elif [ -x "$(command -v dnf)" ];     then  dnf install java-18-openjdk-headless
-elif [ -x "$(command -v pacman)" ]; then pacman -S jre-openjdk-headless --needed
+if [ -x "$(command -v apk)" ];       then  apk add --no-cache openjdk18-jre-headless nano grep procps
+elif [ -x "$(command -v apt-get)" ]; then  apt-get install openjdk-18-jre-headless nano grep procps
+elif [ -x "$(command -v apt)" ];  then  sudo apt install openjdk-18-jre-headless nano grep procps
+elif [ -x "$(command -v dnf)" ];     then  dnf install java-18-openjdk-headless nano grep procps
+elif [ -x "$(command -v pacman)" ]; then pacman -S jre-openjdk-headless nano grep procps --needed
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: Java or check github"
 fi
 }
 
 installer_sudo() {
-if [ -x "$(command -v apk)" ];       then  sudo apk add --no-cache openjdk18-jre-headless
-elif [ -x "$(command -v apt-get)" ];  then  sudo apt-get install openjdk-18-jre-headless openjdk-18-jdk-headless tar
-elif [ -x "$(command -v dnf)" ];     then  sudo dnf install java-18-openjdk-headless
-elif [ -x "$(command -v pacman)" ]; then sudo pacman -S jre-openjdk-headless --needed
+if [ -x "$(command -v apk)" ];       then  sudo apk add --no-cache openjdk18-jre-headless nano grep procps
+elif [ -x "$(command -v apt-get)" ];  then  sudo apt-get install openjdk-18-jre-headless nano grep procps
+elif [ -x "$(command -v apt)" ];  then  sudo apt install openjdk-18-jre-headless nano grep procps
+elif [ -x "$(command -v dnf)" ];     then  sudo dnf install java-18-openjdk-headless nano grep procps
+elif [ -x "$(command -v pacman)" ]; then sudo pacman -S jre-openjdk-headless nano grep procps --needed
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: Java or check github"
 fi
 }
-
+# Detects if you're signed in as root
 if [ $(id -u) -eq 0 ]; then
     installer
 else
     installer_sudo
 fi
-# Download Quilt server jar
+echo "Done! Finalizing and creating shell script.."
 
+# Runs the quilt server installer
 quilt() {
 printf "\nDownloading MC Java server $version.. Powered by Quilt! Please Wait.."
 java -jar quilt-installer-$installer_version.jar \
     install server $version \
     --download-server
 clear
-echo "Done! Finalizing and creating shell script.. (so you should only have to run mc in your terminal.)"
 # Cleanup
-
-cd server && cp -r libraries server.jar quilt-server-launch.jar .. && cd .. && rm -R server
+cd server && cp -r libraries server.jar quilt-server-launch.jar .. && cd .. && rm -R server LICENSE README.md
 }
-
 quilt
 
 mc() {
@@ -70,7 +70,7 @@ fi
 
 mc_root() {
 echo -e "\nYou have *$ram* Mb available!"
-echo -e " \nHow much ram do you want to allocate to your server? (Mb) \n(leave atleast 2000Mb or more for Android. u may experience crashes otherwise.)"
+echo -e " \nHow much memory do you want to allocate to your server? (Mb) \n(leave atleast 2000Mb or more for Android. u may experience crashes otherwise.)"
 read -p $"Mb: " option
 if [[ ! $option =~ ^[0-9]+$ ]]; then
     echo -e "\nPlease enter a number only."
@@ -82,13 +82,12 @@ else
     cd $whereami && java -Xms$option -Xmx$option -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar quilt-server-launch.jar nogui" >> $mc_root && chmod +x $mc_root
 fi
 }
-
 if [ $(id -u) -eq 0 ]; then
     mc_root
 else
     mc
 fi
-# ^ Creates a shell script at /.local/bin or /bin if root. ^
 
-printf "That should be it! \nrun mc in your terminal to start the mc server anytime. \nPlease make sure to exit your session and log back in for it to take effect! \nIf anything is wrong please open an Issue in github, or DM me on Discord. \n~ https://github.com/Theslees/termux-Optimized-MC-Java-server || Theslees\#1162 ~\n "
+printf "That should be it! \nrun \"mc\" in your terminal to start the mc server anytime. \nIf anything is wrong please open an Issue in github, or DM me on Discord. \n~ https://github.com/Theslees/termux-Optimized-MC-Java-server ~ || ~ Theslees\#1162 ~\n "
+return 0
 exit
