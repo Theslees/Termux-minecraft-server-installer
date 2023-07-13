96739323975452b92d897d4f58024947f67f264e
Joey Garcia <MasterGamin4790040@gmail.com>
2023-07-13 00:55:43 -0400

    Fix bugs for termux and more

--- install.sh
+++ install.sh
@@ -3,16 +3,19 @@
 clear
 printf "Setting up...\n "
 printf "currently running script as $USER...\n "
-
 # Welcome to another minecraft server installer! -- Executes with superuser and normal user. finally. https://github.com/Theslees/Termux-minecraft-server-installer
+
 # *Define the variables*
 . /etc/os-release
+# $version used in quilt() to determine what instance version to download
 version="1.20.1"
-# removed installer_version, script doesnt install the quilt server installer locally anymore.
+# removed installer_version, script doesnt store the quilt server installer locally anymore.
 #bashrc variable removed, not used
 ram=$(free --mega | grep Mem | awk '{ print $7 }')
 whereami=$(pwd)
 #distroid removed, not needed
+# $mc and $mc_root are here to help setup the shell script placed in your current enviroment's bin directory for executing within your terminal, like a normal binary.
+# the configuration processes can be found in the mc() and mc_root() functions, for non-root and root respectively.
 mc="$HOME/.local/bin/mc"
 mc_root="/bin/mc"
 termux=/data/data/com.termux/files/usr
@@ -22,35 +25,44 @@ termux=/data/data/com.termux/files/usr
 # - fixed! if openjdk18 is not in your repositories for some reason, it will not error despite not installing properly. fallback to openjdk17.
 # - fixed! if you're running this on termux, it'll go through the normal installer despite already knowing that they wont work. Not a bug, but an oversight causing Termux users having to wait longer for the script to finish.
 # - New! Update to Java 19, and 1.19.3.
+# - New! Update to Java 20 (and 1.20.x i forgot to mention, i'll stop timestamping these (if you can even consider it that, I did not write the time of these writings whatsoever) things here though, its stupid lol)
+# - fixed! Improved termux and root detection algorithms in different parts of this script
 # ---------------------------------------------
 
 # uses $termux to check for a directory normally found in Termux. Detects termux enviroment
-if [ -x "$(command -v $termux)" ]; then
-  echo -n "Detected Termux enviroment! continuing setup..\n "
-  termux=true
-  pkg update && pkg install openjdk-17 grep procps wget
-elif [ $? != 0 ]; then
-  sudo pkg update && sudo pkg install openjdk-17 grep procps wget
+if [ -d $termux ]; then
+  termux=0
+  echo -n "Detected Termux enviroment! continuing setup.."
+    if [ $(whoami) != "root" ]; then
+    echo -n "Installing dependancies.."
+    sudo pkg update && sudo pkg install openjdk-17 grep procps wget
+  else
+    echo -n "Installing dependancies.."
+    pkg update && pkg install openjdk-17 grep procps wget
+  fi
 else
-    return 0
-    termux=false
+  termux=1
+	echo -n "Detected a non-termux enviroment, continuing setup! \n"
 fi
+
 
 installer() {
 # Download dependencies and requirements
-if [ $termux = true ]; then break 2
-else return 0
+if [ $termux = 0 ]; then 
+  break 
+else 
+  return 0
 fi
 
 if [ -x "$(command -v apk)" ]; then
   pkgfnd=1
-    apk update && apk add --no-cache openjdk19-jre-headless nano grep procps wget
+    apk update && apk add --no-cache openjdk20-jre-headless nano grep procps wget
 elif [ -x "$(command -v apt-get)" ]; then
   pkgfnd=2
-    apt-get update && apt-get install openjdk-19-jre-headless nano grep procps wget
+    apt-get update && apt-get install openjdk-20-jre-headless nano grep procps wget
 elif [ -x "$(command -v apt)" ]; then
   pkgfnd=3
-    apt update && apt install openjdk-19-jre-headless nano grep procps wget
+    apt update && apt install openjdk-20-jre-headless nano grep procps wget
 elif [ -x "$(command -v dnf)" ]; then
   pkgfnd=4
     dnf upgrade && dnf install java-latest-openjdk-headless nano grep procps-ng wget
@@ -63,7 +75,7 @@ else
   exit
 fi
 
-if [ $? = 127 ]; then
+if [ $? != 0 ]; then
   pkgfail=true
 else
   pkgfail=false
@@ -92,18 +104,20 @@ fi
 }
 
 installer_sudo() {
-if [ $termux = true ]; then break 2
-else return 0
+if [ $termux = 0 ]; then 
+  break
+else 
+  return 0
 fi
 if [ -x "$(command -v apk)" ]; then
   pkgfnd=1
-  sudo apk update && sudo apk add --no-cache openjdk19-jre-headless nano grep procps wget
+  sudo apk update && sudo apk add --no-cache openjdk20-jre-headless nano grep procps wget
 elif [ -x "$(command -v apt-get)" ]; then
   pkgfnd=2
-  sudo apt-get update && sudo apt-get install openjdk-19-jre-headless nano grep procps
+  sudo apt-get update && sudo apt-get install openjdk-20-jre-headless nano grep procps
 elif [ -x "$(command -v apt)" ]; then
   pkgfnd=3
-  sudo apt update && sudo apt install openjdk-19-jre-headless nano grep procps
+  sudo apt update && sudo apt install openjdk-20-jre-headless nano grep procps
 elif [ -x "$(command -v dnf)" ]; then
   pkgfnd=4
   sudo dnf upgrade && sudo dnf install java-latest-openjdk-headless nano grep procps-ng
@@ -116,7 +130,7 @@ else
   exit
 fi
 
-if [ $? = 127 ]; then
+if [ $? != 0 ]; then
   pkgfail=true
 else
   pkgfail=false
@@ -144,7 +158,7 @@ fi
 }
 
 # Detects if you're root, then runs the appropriate setup
-if [ $(id -u) -eq 0 ]; then
+if [ $(whoami) == "root" ]; then
     installer
 else
     installer_sudo
@@ -154,33 +168,34 @@ fi
 quilt() {
 clear
 printf "\nDownloading MC Java server $version.. Powered by Quilt! Please Wait.. \n "
-wget https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/0.5.1/quilt-installer-0.5.1.jar
-java -jar quilt-installer-0.5.1.jar \
+# This will get the quilt server installer jar and run it, which downloads the prerequisites required for the server instance itself and the jar files to boot it.
+wget https://quiltmc.org/api/v1/download-latest-installer/java-universal&&mv java-universal quilt-installer.jar
+java -jar quilt-installer.jar \
     install server $version \
     --download-server
-# Cleanup
+# Cleanup and organize quilt files
 clear
 printf "Done! Cleaning and organizing.. \n "
-cd server && mv libraries server.jar quilt-server-launch.jar .. && cd .. && rm -R server LICENSE README.md quilt-installer-0.5.1.jar
+mv server/* . && rm -R server quilt-installer.jar
 }
 quilt
 
 mc() {
 clear
-echo -e "\nYou have *$ram* Mb available!"
-echo -e " \nHow much ram do you want to allocate to your server? (Mb) \n(leave atleast 2000Mb or more for your operating system. u may experience crashes otherwise.)"
+printf "You have *$ram* Mb available!\n"
+printf "How much ram do you want to allocate to your server? (Mb) \n(leave atleast 2000Mb or more for your operating system. u may experience crashes otherwise.)\n"
+printf "note: if the script exits because a number wasn't placed, then it'll just exit, please run the script again if this happens, you should be fine. \n(no decimals allowed either please)"
 read -p $"Mb: " option
 if [[ ! $option =~ ^[0-9]+$ ]]; then
     echo -e "\nPlease enter a number only."
     return 127
     break 1
 else
-    if [ $termux = true ]; then 
+    if [ $termux = 0 ]; then 
       mc="/data/data/com.termux/files/usr/bin"
     else 
       continue
     fi
-    clear
     option="${option}M"
     touch $mc && cat /dev/null > $mc && echo "#!/bin/bash
     cd $whereami && java -Xms$option -Xmx$option -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar quilt-server-launch.jar nogui" >> $mc && chmod +x $mc
@@ -189,27 +204,27 @@ fi
 
 mc_root() {
 clear
-echo -e "\nYou have *$ram* Mb available!"
-echo -e " \nHow much memory do you want to allocate to your server? (Mb) \n(leave atleast 2000Mb or more for your operating system. u may experience crashes otherwise.)"
+printf "You have *$ram* Mb available!\n "
+printf "How much memory do you want to allocate to your server? (Mb) \n(leave atleast 2000Mb free or more for your operating system. u may experience crashes otherwise.)\n"
+printf "note: if the script exits because a number wasn't placed, then it'll just exit, please run the script again if this happens, you should be fine. \n(no decimals allowed either please)"
 read -p $"Mb: " option
 if [[ ! $option =~ ^[0-9]+$ ]]; then
     echo -e "\nPlease enter a number only."
     return 127
     break 1
 else
-    if [ $termux = true ]; then 
+    if [ $termux = 0 ]; then 
       mc_root="/data/data/com.termux/files/usr/bin"
     else 
       continue
     fi
-    clear
     option="${option}M"
     touch $mc_root && cat /dev/null > $mc_root && echo "#!/bin/bash
     cd $whereami && java -Xms$option -Xmx$option -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar quilt-server-launch.jar nogui" >> $mc_root && chmod +x $mc_root
 fi
 }
 
-if [ $(id -u) -eq 0 ]; then
+if [ $(whoami) == "root" ]; then
     mc_root
 else
     mc
